afb52ca64d69736db446d351d3cd54e364575931
Leonardo Velazquez <leo.vel330@gmail.com>
2022-09-15 15:02:40 -0500

    Entidades y el acceso a datos hechos

---
+++ AccesoDatosTienda/AccesoTienda.cs
@@ -0,0 +1,29 @@
+﻿using ConectarRedReal;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccesoDatosTienda
+{
+    public class AccesoTienda : IEntidades
+    {
+        Base b = new Base("localhost", "root", "", "ajedrez");
+        public void Borrar(dynamic Entidad)
+        {
+            string.Format("call insertarproductos({0},'{1}','{2}',{3});", Entidad.idProducto,Entidad.Nombre,Entidad.Descripcion,Entidad.Precio);
+        }
+
+        public void Guardar(dynamic Entidad)
+        {
+            string.Format("call insertarproductos({0});",Entidad.idProducto);
+        }
+
+        public DataSet Mostrar(string filtro)
+        {
+            return b.Obtener(string.Format("call showproductos('%{0}%')",filtro),"producto");
+        }
+    }
+}
---
+++ AccesoDatosTienda/IEntidades.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccesoDatosTienda
+{
+    interface IEntidades
+    {
+        void Guardar(dynamic Entidad);
+        void Borrar(dynamic Entidad);
+        DataSet Mostrar(string filtro);
+    }
+}
